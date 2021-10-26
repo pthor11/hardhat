ea40b400290ac3ce03ed35f3b7ae7f3d69873ed8
phuonglm <phuonglm@me.com>
2021-10-26 16:55:52 +0700

    update runes deploy script

--- scripts/runes.deploy.ts
+++ scripts/runes.deploy.ts
@@ -6,25 +6,29 @@ async function main() {
 
     const RunesContract = await ethers.getContractFactory("Runes");
 
+    const rune_contracts: any[] = []
+
     for (const rune of RUNES) {
         const rune_contract = await RunesContract.deploy(rune, rune);
 
         await rune_contract.deployed();
 
+        rune_contracts.push(rune_contract)
+
         console.log("Deployed", rune, rune_contract.address);
-
-        console.log(`Verifying ...`);
-
-        await run('verify:verify', {
-            address: rune_contract.address,
-            constructorArguments: [
-                rune,
-                rune
-            ]
-        })
-
-        console.log(`Verified`);
     }
+
+    console.log(`Verifying ...`);
+
+    await run('verify:verify', {
+        address: rune_contracts[0].address,
+        constructorArguments: [
+            RUNES[0],
+            RUNES[0]
+        ]
+    })
+
+    console.log(`Verified`);
 }
 
 main()
